1f09e1faa6649e2f5e736fe0643ab207464270c8
124 <nagatosaw@gmail.com>
2021-11-24 09:48:56 +0200

    initiwl

--- 1-Fix.py
+++ 1-Fix.py
@@ -1,4 +1,4 @@
-first_num = 2
-second_num = 3
-sum = first_num + second_num
-print(sum)
+first_num = 2  # removed semicolon
+second_num = 3  # removed semicolon + typo
+sum = first_num + second_num  # removed semicolon + typo
+print(sum)  # removed semicolon
